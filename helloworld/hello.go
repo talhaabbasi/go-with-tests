61b6f28aec411af71adba3bcdf90e26936fb97e0
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-06-18 11:54:59 +0500

    [FEAT] Add handling for spanish language :muscle:

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -3,19 +3,24 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "Spanish"
 
 func Hello() string {
 	return englishHelloPrefix + "World"
 }
 
-func HelloPerson(name string) string {
+func HelloPerson(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
 	fmt.Println(Hello())
-	fmt.Println(HelloPerson("Talha"))
+	fmt.Println(HelloPerson("Talha", ""))
 }
--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -13,15 +13,22 @@ func TestHello(t *testing.T) {
 
 func TestHelloPerson(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := HelloPerson("Talha")
+		got := HelloPerson("Talha", "")
 		want := "Hello, Talha"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello with empty string", func(t *testing.T) {
-		got := HelloPerson("")
+		got := HelloPerson("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello in Spanish", func(t *testing.T) {
+		got := HelloPerson("Talha", "Spanish")
+		want := "Hola, Talha"
 
 		assertCorrectMessage(t, got, want)
 	})
