8f4307aaa3d2a030e5e90ce542c56a4e5b232853
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-06-20 13:56:33 +0500

    [FEAT] Incorporate loops :muscle:

--- repeater/repeat.go
+++ repeater/repeat.go
@@ -2,5 +2,9 @@ package iterations
 
 // Repeat takes one letter and returns it 5 times
 func Repeat(letter string) string {
-	return "aaaaa"
+	var repeated string
+	for i := 0; i < 5; i++ {
+		repeated = repeated + letter
+	}
+	return repeated
 }
