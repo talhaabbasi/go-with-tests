b0e75197d0acb340aecf77f5712d03aae3c5c161
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-06-20 13:33:10 +0500

    [FEAT] Add godocs and comments :muscle:

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	got := Add(2, 2)
@@ -9,4 +12,10 @@ func TestAdder(t *testing.T) {
 	if got != want {
 		t.Errorf("got '%q' want '%q'", got, want)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
