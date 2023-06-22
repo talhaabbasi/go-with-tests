92a2ea95f31b36621a7f8d2af38c584352e3a64d
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-06-22 22:56:19 +0500

    [FEAT] Add benchmark :muscle:

--- repeater/repeat_test.go
+++ repeater/repeat_test.go
@@ -14,6 +14,12 @@ func TestRepeat(t *testing.T) {
 	}
 }
 
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
 func ExampleRepeat() {
 	repeats := Repeat("a")
 	fmt.Println(repeats)
