38e648653580def75404aa1657541adb3d1f1a79
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-06-22 22:54:33 +0500

    [REFACTOR] Add and assignment operator :fire:

--- repeater/repeat.go
+++ repeater/repeat.go
@@ -4,7 +4,7 @@ package iterations
 func Repeat(letter string) string {
 	var repeated string
 	for i := 0; i < 5; i++ {
-		repeated = repeated + letter
+		repeated += letter
 	}
 	return repeated
 }
