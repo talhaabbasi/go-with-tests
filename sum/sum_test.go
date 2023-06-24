4cd9ac832ad3c2f4983debc70230fc8ab63a93b7
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-06-24 11:55:00 +0500

    [FEAT] Add sliced array :muscle:

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,12 +3,25 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("expected %d but got %d, given %v", want, got, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of n numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
