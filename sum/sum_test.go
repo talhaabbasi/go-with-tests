95f2a2b25e9fe198a8bfc7bd3d6a8d4a470cf879
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-07-02 19:20:21 +0500

    [TEST] Add helper function :gun:

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -40,10 +40,21 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	got := SumAllTails([]int{1, 2, 3}, []int{3, 4, 5})
-	want := []int{5, 9}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
 	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{3, 4, 5})
+		want := []int{5, 9}
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
 }
