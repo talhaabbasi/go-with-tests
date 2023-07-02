b7d3caae7adaf2471385d21d8053c7fc3c105c47
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-07-02 18:47:00 +0500

    [FEAT] Add SumAll function :muscle:

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -24,4 +27,14 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 29})
+	want := []int{3, 29}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
