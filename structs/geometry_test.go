5c986a78c6d9c539d7247032e9001403b11d2ac6
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-07-04 19:00:18 +0500

    [REFACTOR] Add interfaces :recycle:

--- structs/geometry_test.go
+++ structs/geometry_test.go
@@ -13,22 +13,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("calculate the area of rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
 	})
-	t.Run("calculate the area of circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
