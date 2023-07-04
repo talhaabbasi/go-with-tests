06d30a4204090de2c93f80ef2c0d5f3bfe8d2b85
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-07-04 18:53:00 +0500

    [FIX] Failing tests :bug:

--- structs/geometry.go
+++ structs/geometry.go
@@ -1,5 +1,7 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -14,7 +16,7 @@ type Circle struct {
 }
 
 func (c Circle) Area() float64 {
-	return 0
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
