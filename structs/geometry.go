b2728cf05e419ac1f8b41cf5073b0c013b48724e
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-07-04 19:14:04 +0500

    [FEAT] Add triangle shape :muscle:

--- structs/geometry.go
+++ structs/geometry.go
@@ -19,6 +19,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+
 type Shape interface {
 	Area() float64
 }
