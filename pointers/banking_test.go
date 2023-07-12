6e9f30870c53eea5beb04eaa2078b0addd07a025
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-07-12 10:04:14 +0500

    [REFACTOR] Add tests :recycle:

--- pointers/banking_test.go
+++ pointers/banking_test.go
@@ -3,27 +3,24 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit function in wallet", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
 		got := wallet.Balance()
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+	t.Run("deposit function in wallet", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw function in wallet", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
