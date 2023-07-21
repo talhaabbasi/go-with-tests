5f986c69ee4500ad7d234e312ebcc96d7da92c15
Talha Abbasi <talhaabbasi1997@gmail.com>
2023-07-21 11:28:47 +0500

    [FEAT] Check for non-existing keys :muscle:

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,13 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (dictionary Dictionary) Search(keyword string) string {
-	return dictionary[keyword]
+func (dictionary Dictionary) Search(keyword string) (string, error) {
+	definition, ok := dictionary[keyword]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,10 +4,22 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assetStrings(t, got, want)
+		assetStrings(t, got, want)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assetStrings(t, err.Error(), want)
+	})
 }
 
 func assetStrings(t testing.TB, got, want string) {
